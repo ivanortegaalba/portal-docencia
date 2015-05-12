e2f97d74a639d941b1aa4b725b3f7d9bbea8246e
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2015-05-12 11:26:46 +0200

    12-05-2015_11:26:46

--- backup-personal/copia_seguridad_drupal.sh
+++ backup-personal/copia_seguridad_drupal.sh
@@ -2,3 +2,4 @@ FECHA=$(date +"%d-%m-%Y_%H:%M")
 sudo mkdir ./$FECHA
 sudo /Applications/XAMPP/bin/mysqldump -u root  drupal-alumnos > ./$FECHA/gestion-docencia.sql
 sudo /Applications/XAMPP/bin/mysqldump -u root  drupal-revisiones > ./$FECHA/revision-trabajos.sql
+git add -A
